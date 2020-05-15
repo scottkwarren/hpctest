685c855cc5cc02f93a69c88838e296c39aa2cd3a
Scott K. Warren <scott@rice.edu>
2020-05-15 18:56:03 -0500

    Add Marty's 'catchthrow' unit test.

---
+++ tests/unit-test/catchthrow/package.py
@@ -0,0 +1,20 @@
+#=========================#
+# CATCHTHROW PACKAGE FILE #
+#=========================#
+
+
+from spack import *
+
+class Catchthrow(MakefilePackage):
+
+    version('1.0', 'hpctest/tests/unit-tests/catchthrow')
+
+    def install(self, spec, prefix):
+    
+        mkdirp(prefix.bin)
+        install('catchthrow', prefix.bin)
+
+
+
+
+
