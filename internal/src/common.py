b3525765917b84af10be4b8690e2d8284c393a2c
Scott K Warren <scott@rice.edu>
2022-04-29 14:32:14 -0500

    Bump version number to 1.1b1 to match git version (plus beta).

--- internal/src/common.py
+++ internal/src/common.py
@@ -47,7 +47,7 @@
 
 
 # manually maintained version id for hpctest
-version = "1.0"
+version = "1.1b1"
 
 # magic cookie shielding 'hpctest _runOne' from civilians
 magic_cookie = "Arma virumque cano Troiae qui primus ab oris"
